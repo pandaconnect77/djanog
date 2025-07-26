580635033cd1479eca3bcf1e9b64351712b0d4b0
Panda Connect <122888167+pandaconnect77@users.noreply.github.com>
2025-07-26 21:57:52 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
-set -o errexit  # Exit on error
+# Used by Render to install dependencies
 
-# Install dependencies
 pip install -r requirements.txt
-
-# Apply DB migrations
-python manage.py migrate
-
-# Collect static files for production
-python manage.py collectstatic --noinput
+python manage.py collectstatic --no-input
